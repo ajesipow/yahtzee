8ed2a9a54cee77d204b807bf635372ae3aecd4da
alexjesipow <alexander.jesipow@gmail.com>
2022-02-19 11:48:50 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod ui;
 
 use crate::app::{AppState, Event, InputMode};
 use crate::score::Score;
-use crate::ui::MenuItem;
-use crossterm::event::KeyCode;
+use crate::ui::render_app;
+use crossterm::event::{KeyCode, KeyEvent};
 use crossterm::terminal::disable_raw_mode;
 use crossterm::{
     event::{self, Event as CEvent},
@@ -14,30 +14,24 @@ use crossterm::{
 };
 use std::io;
 use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, SendError};
 use std::thread;
 use std::time::{Duration, Instant};
 use tui::backend::{Backend, CrosstermBackend};
-use tui::layout::{Constraint, Direction, Layout};
-use tui::style::{Color, Modifier, Style};
-use tui::text::{Span, Spans};
-use tui::widgets::{Block, BorderType, Borders, List, ListItem, Paragraph, Tabs};
 use tui::Terminal;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    enable_raw_mode().expect("can run in raw mode");
-
+fn event_loop(tick_rate: Duration) -> Receiver<Event<KeyEvent>> {
     let (tx, rx) = mpsc::channel();
-    let tick_rate = Duration::from_millis(200);
-    thread::spawn(move || {
+    thread::spawn(move || -> Result<(), SendError<Event<KeyEvent>>> {
         let mut last_tick = Instant::now();
         loop {
             let timeout = tick_rate
                 .checked_sub(last_tick.elapsed())
                 .unwrap_or_else(|| Duration::from_secs(0));
 
-            if event::poll(timeout).expect("poll works") {
-                if let CEvent::Key(key) = event::read().expect("can read events") {
-                    tx.send(Event::Input(key)).expect("can send events");
+            if event::poll(timeout).unwrap_or(false) {
+                if let Ok(CEvent::Key(key)) = event::read() {
+                    tx.send(Event::Input(key))?;
                 }
             }
 
@@ -48,84 +42,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     });
+    rx
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode()?;
+    let rx = event_loop(Duration::from_millis(200));
 
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
-
-    let menu_titles = vec!["New game", "Roll dice", "Quit"];
-    let active_menu_item = MenuItem::NewGame;
     let mut app_state = AppState::new();
 
     loop {
-        terminal.draw(|rect| {
-            let size = rect.size();
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(2)
-                .constraints(
-                    [
-                        Constraint::Length(3),
-                        Constraint::Length(3),
-                        Constraint::Length(3),
-                        Constraint::Min(2),
-                    ]
-                    .as_ref(),
-                )
-                .split(size);
-
-            let menu = menu_titles
-                .iter()
-                .map(|t| {
-                    let (first, rest) = t.split_at(1);
-                    Spans::from(vec![
-                        Span::styled(
-                            first,
-                            Style::default()
-                                .fg(Color::Yellow)
-                                .add_modifier(Modifier::UNDERLINED),
-                        ),
-                        Span::styled(rest, Style::default().fg(Color::White)),
-                    ])
-                })
-                .collect();
-
-            let tabs = Tabs::new(menu)
-                .select(active_menu_item.clone().into())
-                .block(Block::default().title("Menu").borders(Borders::ALL))
-                .style(Style::default().fg(Color::White))
-                .highlight_style(Style::default().fg(Color::Yellow))
-                .divider(Span::raw("|"));
-
-            let dice_paragraph = Paragraph::new(
-                app_state
-                    .dice_state
-                    .dice
-                    .0
-                    .iter()
-                    .map(|v| v.to_string())
-                    .collect::<Vec<String>>()
-                    .join(" "),
-            )
-            .style(Style::default().add_modifier(Modifier::BOLD))
-            .block(Block::default().borders(Borders::ALL).title(format!(
-                "Dice - roll {:?} / {:?}",
-                app_state.dice_state.number_of_rolls, app_state.dice_state.max_number_of_rolls
-            )));
-
-            let input = Paragraph::new(app_state.selection_input.as_ref())
-                .style(match app_state.input_mode {
-                    InputMode::Normal => Style::default(),
-                    InputMode::Selecting => Style::default().fg(Color::Yellow),
-                })
-                .block(Block::default().borders(Borders::ALL).title("Selection"));
-
-            rect.render_widget(tabs, chunks[0]);
-            rect.render_widget(dice_paragraph, chunks[1]);
-            rect.render_widget(input, chunks[2]);
-            rect.render_widget(render_score(&app_state.score), chunks[3]);
-        })?;
+        terminal.draw(|rect| render_app(&app_state, rect))?;
 
         match rx.recv()? {
             Event::Input(event) => match app_state.input_mode {
@@ -176,202 +107,4 @@ fn quit_app<B: Backend>(mut terminal: Terminal<B>) -> Result<(), Box<dyn std::er
     terminal.show_cursor()?;
     terminal.clear()?;
     Ok(())
-}
-
-fn render_score<'a>(score: &Score) -> List<'a> {
-    let scores = Block::default()
-        .borders(Borders::ALL)
-        .style(Style::default().fg(Color::White))
-        .title("Score")
-        .border_type(BorderType::Plain);
-
-    let mut items: Vec<_> = vec![];
-    items.extend(vec![
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Aces: {}",
-                score
-                    .upper_section
-                    .aces
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Twos: {}",
-                score
-                    .upper_section
-                    .twos
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Threes: {}",
-                score
-                    .upper_section
-                    .threes
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Fours: {}",
-                score
-                    .upper_section
-                    .fours
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Fives: {}",
-                score
-                    .upper_section
-                    .fives
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Sixes: {}",
-                score
-                    .upper_section
-                    .sixes
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Total without bonus: {}",
-                score.upper_section.score_without_bonus().to_string(),
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Bonus: {}",
-                score
-                    .upper_section
-                    .bonus
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Total upper section: {}",
-                score.upper_section.total_score().to_string(),
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Three of a kind: {}",
-                score
-                    .lower_section
-                    .three_of_a_kind
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Four of a kind: {}",
-                score
-                    .lower_section
-                    .four_of_a_kind
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Full house: {}",
-                score
-                    .lower_section
-                    .full_house
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Small straight: {}",
-                score
-                    .lower_section
-                    .small_straight
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Large straight: {}",
-                score
-                    .lower_section
-                    .large_straight
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Yahtzee: {}",
-                score
-                    .lower_section
-                    .yahtzee
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Chance: {}",
-                score
-                    .lower_section
-                    .chance
-                    .clone()
-                    .map_or("-".to_string(), |v| v.to_string())
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!(
-                "Total lower section: {}",
-                score.lower_section.total_score().to_string(),
-            ),
-            Style::default(),
-        )])),
-        ListItem::new(Spans::from(vec![Span::styled(
-            format!("TOTAL SCORE: {}", score.total_score().to_string(),),
-            Style::default(),
-        )])),
-    ]);
-
-    List::new(items).block(scores).highlight_style(
-        Style::default()
-            .bg(Color::Yellow)
-            .fg(Color::Black)
-            .add_modifier(Modifier::BOLD),
-    )
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,224 @@
-#[derive(Copy, Clone, Debug)]
-pub(crate) enum MenuItem {
-    NewGame,
+use crate::{AppState, InputMode, Score};
+use tui::backend::Backend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::style::{Color, Modifier, Style};
+use tui::text::{Span, Spans};
+use tui::widgets::{Block, BorderType, Borders, List, ListItem, Paragraph, Tabs};
+use tui::Frame;
+
+pub(crate) fn render_app<B: Backend>(app_state: &AppState, rect: &mut Frame<B>) {
+    let menu_titles = vec!["New game", "Roll dice", "Quit"];
+
+    let size = rect.size();
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints(
+            [
+                Constraint::Length(3),
+                Constraint::Length(3),
+                Constraint::Length(3),
+                Constraint::Min(2),
+            ]
+            .as_ref(),
+        )
+        .split(size);
+
+    let menu = menu_titles
+        .iter()
+        .map(|t| {
+            let (first, rest) = t.split_at(1);
+            Spans::from(vec![
+                Span::styled(
+                    first,
+                    Style::default()
+                        .fg(Color::Yellow)
+                        .add_modifier(Modifier::UNDERLINED),
+                ),
+                Span::styled(rest, Style::default().fg(Color::White)),
+            ])
+        })
+        .collect();
+
+    let tabs = Tabs::new(menu)
+        .block(Block::default().title("Menu").borders(Borders::ALL))
+        .style(Style::default().fg(Color::White))
+        .highlight_style(Style::default().fg(Color::Yellow))
+        .divider(Span::raw("|"));
+
+    let dice_paragraph = Paragraph::new(
+        app_state
+            .dice_state
+            .dice
+            .0
+            .iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>()
+            .join(" "),
+    )
+    .style(Style::default().add_modifier(Modifier::BOLD))
+    .block(Block::default().borders(Borders::ALL).title(format!(
+        "Dice - roll {:?} / {:?}",
+        app_state.dice_state.number_of_rolls, app_state.dice_state.max_number_of_rolls
+    )));
+
+    let input = Paragraph::new(app_state.selection_input.as_ref())
+        .style(match app_state.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Selecting => Style::default().fg(Color::Yellow),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Selection"));
+
+    rect.render_widget(tabs, chunks[0]);
+    rect.render_widget(dice_paragraph, chunks[1]);
+    rect.render_widget(input, chunks[2]);
+    rect.render_widget(render_score(&app_state.score), chunks[3]);
 }
 
-impl From<MenuItem> for usize {
-    fn from(input: MenuItem) -> usize {
-        match input {
-            MenuItem::NewGame => 0,
-        }
-    }
+fn render_score<'a>(score: &Score) -> List<'a> {
+    let scores = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().fg(Color::White))
+        .title("Score")
+        .border_type(BorderType::Plain);
+
+    let mut items: Vec<_> = vec![];
+    items.extend(vec![
+        list_item(format!(
+            "Aces: {}",
+            score
+                .upper_section
+                .aces
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Twos: {}",
+            score
+                .upper_section
+                .twos
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Threes: {}",
+            score
+                .upper_section
+                .threes
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Fours: {}",
+            score
+                .upper_section
+                .fours
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Fives: {}",
+            score
+                .upper_section
+                .fives
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Sixes: {}",
+            score
+                .upper_section
+                .sixes
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Total upper section without bonus: {}",
+            score.upper_section.score_without_bonus().to_string(),
+        )),
+        list_item(format!(
+            "Bonus: {}",
+            score
+                .upper_section
+                .bonus
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Total upper section: {}",
+            score.upper_section.total_score().to_string(),
+        )),
+        list_item(format!(
+            "Three of a kind: {}",
+            score
+                .lower_section
+                .three_of_a_kind
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Four of a kind: {}",
+            score
+                .lower_section
+                .four_of_a_kind
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Full house: {}",
+            score
+                .lower_section
+                .full_house
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Small straight: {}",
+            score
+                .lower_section
+                .small_straight
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Large straight: {}",
+            score
+                .lower_section
+                .large_straight
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Yahtzee: {}",
+            score
+                .lower_section
+                .yahtzee
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Chance: {}",
+            score
+                .lower_section
+                .chance
+                .clone()
+                .map_or("-".to_string(), |v| v.to_string())
+        )),
+        list_item(format!(
+            "Total lower section: {}",
+            score.lower_section.total_score().to_string(),
+        )),
+        list_item(format!("TOTAL SCORE: {}", score.total_score().to_string(),)),
+    ]);
+
+    List::new(items).block(scores).highlight_style(
+        Style::default()
+            .bg(Color::Yellow)
+            .fg(Color::Black)
+            .add_modifier(Modifier::BOLD),
+    )
+}
+
+fn list_item<'a>(content: String) -> ListItem<'a> {
+    ListItem::new(Spans::from(vec![Span::styled(content, Style::default())]))
 }
